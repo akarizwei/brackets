de569908a31a3f1a8fcf729e47289b104c49e974
Akarizwei <andy.b.sky@yandex.ru>
2019-10-08 23:11:09 +0400

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    Array.prototype.last = function () {
+        return this[this.length - 1];
+    };
+
+    let matchingOpeningBracket, ch;
+    let stack = [];
+
+    let openingBrackets = [],
+        closingBrackets = [];
+
+    for (let j = 0; j < bracketsConfig.length; j++) {
+        let bracketArray = bracketsConfig[j];
+        openingBrackets.push(bracketArray[0]);
+        closingBrackets.push(bracketArray[1]);
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        ch = str[i];
+
+        if (closingBrackets.indexOf(ch) == -1 && openingBrackets.indexOf(ch) > -1) {
+            stack.push(ch);
+        } else if (closingBrackets.indexOf(ch) > -1 && openingBrackets.indexOf(ch) > -1) {
+            if (stack.length == 0 || stack.last() != ch) {
+                stack.push(ch)
+            } else {
+                stack.pop();
+            }
+        } else if (closingBrackets.indexOf(ch) > -1 && openingBrackets.indexOf(ch) == -1) {
+            matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)];
+            if (stack.length == 0 || (stack.pop() != matchingOpeningBracket)) {
+                return false
+            }
+        }
+    }
+
+    return (stack.length == 0)
+
 }
